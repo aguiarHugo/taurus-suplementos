7f51d38e4ccaae7158263fe8f3a58e13c29a44cf
aguiarHugo <aguiarhugofx@gmail.com>
2023-09-26 08:58:13 -0300

    imported components

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,19 +1,29 @@
+import Cards from '@/components/Cards'
+import Filters from '@/components/Filters'
 import SearchForm from '@/components/SearchForm'
-import React from 'react'
+interface Props {
+  searchParams: { [key:string]: string | undefined }
+}
 
-type Props = {}
+const Page = async ({ searchParams } : Props) => {
+  // const resources = await getResources({
+  //   query: '',
+  //   category: searchParams?.category || '',
+  //   page: '1'
+  // })
 
-const page = (props: Props) => {
   return (
     <main className='flex-center sm:p-16 xs:p-8 px-6 py-12 max-auto w-full max-w-screen-2xl flex-col'>
       <section className='w-full pt-[98px]'>
-        <div className='flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
+        <div className='flex-center relative min-h-[350px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
           <h1 className='sm:heading1 heading2 mb-6 text-center text-gradient_red'>Taurus Suplementos</h1>
         </div>
         <SearchForm />
+        <Filters />
+        <Cards />
       </section>
     </main>
   )
 }
 
-export default page
+export default Page
